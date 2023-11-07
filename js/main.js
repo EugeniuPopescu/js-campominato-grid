e89cc62070bf04d564fa960cf74f83f080210c94
Eugen <epopescuu@yahoo.it>
2023-11-07 17:58:37 +0100

    perf(style.css, main.js) add number on squares

--- js/main.js
+++ js/main.js
@@ -12,30 +12,38 @@ play.addEventListener('click', function() {
     
     // FOR che mi cicla da 1 cella a nCells che ho impostato
     for (let i = 1; i <= nCells; i++) {
+
         
-        // creo il mio div
-        const cell = document.createElement('div');
-
-        // aggiungo la mia classe al div
-        cell.classList.add('square');
-
+        let cell = createSquare(i);
         // metto il div nel mio cell nella dabella grid
         grid.appendChild(cell);
-
-        // creo il mio 
-
-
-        // aggiungo un addEvenListener per ogni cella al click
-        // voglio gestire ogni quadrato al click
-        cell.addEventListener('click', function () {
-
-            // al console log fa vedere che cella clicko
-            console.log('cell N:', i);
-
-            // this (si riferisce all'elemento dell'addEvenetListener)
-            //  toggle (aggiunge e toglie la classe da un elemento)
-            this.classList.toggle('highlight');
-        });
         
+        // mi segna il numero della cella
+        cell.innerHTML = i;
     }
 });
+
+
+function createSquare(i) {
+    // creo il mio div
+    const cell = document.createElement('div');
+
+    // aggiungo la mia classe al div
+    cell.classList.add('square');
+
+    // aggiungo un addEvenListener per ogni cella al click
+    // voglio gestire ogni quadrato al click
+    cell.addEventListener('click', function () {
+
+        // al console log fa vedere che cella clicko
+        console.log('cell N:', i);
+
+
+        // this (si riferisce all'elemento dell'addEvenetListener)
+        //  toggle (aggiunge e toglie la classe da un elemento)
+        this.classList.toggle('highlight');
+    });
+
+    // return 
+    return cell;
+}
